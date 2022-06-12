ef6f30dd81bbd74414e292dd52ccd0e17e8367d7
jieun <stopss417@gmail.com>
2022-06-12 16:18:46 +0900

    setting

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('hi');
+});
+
+app.use(express.json());
+app.use('/api', express.urlencoded({ extended: false }), router);
+app.listen(3000, () => {
+    console.log('서버가 요청을 받을 준비가 됐어요');
+});
