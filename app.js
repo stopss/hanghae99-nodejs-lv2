fb2bdcb52b28a707415dad60d15922d0da3957b9
jieun <stopss417@gmail.com>
2022-06-12 16:54:35 +0900

    Add User DB

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 const { Op } = require('sequelize');
 const jwt = require('jsonwebtoken');
 
+dotenv.config();
 const app = express();
 const router = express.Router();
 
@@ -9,6 +12,7 @@ router.get('/', (req, res) => {
     res.send('hi!');
 });
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', express.urlencoded({ extended: false }), router);
 app.listen(3000, () => {
---
+++ migrations/20220612074738-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      userId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      nickname: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      admin: {
+        defaultValue: false,
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
